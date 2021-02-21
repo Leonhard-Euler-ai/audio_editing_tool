f2dd5ab2b9e67ebc32df718ba2d6e1af248769fa
fantasy-account <77471735+fantasy-account@users.noreply.github.com>
2021-02-21 23:53:18 +0800

    更新了主界面，上方添加了导航栏

--- src/views/main/MainView.vue
+++ src/views/main/MainView.vue
@@ -1,26 +1,32 @@
 <template>
 <div>
+  <MainViewHeader/>
   <p>主页</p>
   <button @click="logout">取消登录</button>
 </div>
 </template>
 
 <script>
-export default {
-  name: "Main",
-  methods:{
-    logout() {
-      window.sessionStorage.removeItem('authorization')
-      this.$store.dispatch('logout').then(res=>{
-        this.$message.info({
-          message:res,
-          offset:200
+  import MainViewHeader from "./MainViewComps/MainViewHeader";
+
+  export default {
+    name: "Main",
+    components: {
+      MainViewHeader
+    },
+    methods: {
+      logout() {
+        window.sessionStorage.removeItem('authorization')
+        this.$store.dispatch('logout').then(res => {
+          this.$message.info({
+            message: res,
+            offset: 200
+          })
+          this.$router.replace('/login')
         })
-        this.$router.replace('/login')
-      })
+      }
     }
   }
-}
 </script>
 
 <style scoped>
---
+++ src/views/main/MainViewComps/MainViewHeader.vue
@@ -0,0 +1,97 @@
+<template>
+  <header class="clearFix" style="padding: 0 100px">
+    <div class="logo">
+      <img src="https://www.ommdata.com/images/upload/websiteLogo/20210219/1613702757354T0CaKD.png" alt="文思海辉教育平台">
+    </div>
+    <div class="menu">
+      <ul>
+        <li>
+          <a href="#">首页</a>
+        </li>
+        <li>
+          <a href="#">在线课程</a>
+        </li>
+        <li>
+          <a href="#">直播</a>
+        </li>
+        <li>
+          <a href="#">在线测评</a>
+        </li>
+        <li>
+          <a href="#">讲师</a>
+        </li>
+        <li>
+          <a href="#">更多</a>
+        </li>
+      </ul>
+      <ul style="float:right">
+        <li class="search">
+          <input type="text" placeholder="搜索...">
+        </li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: "MainViewHeader"
+  }
+</script>
+
+<style scoped>
+  .logo {
+    float: left;
+    font-size: 36px;
+    line-height: 45px;
+    width: 100px;
+    height: 66px;
+    margin: 0 auto;
+    position: relative;
+  }
+
+  .logo img {
+    width: auto;
+    text-align: center;
+    vertical-align: middle;
+  }
+
+  li {
+    float: left;
+  }
+
+  li a {
+    background-color: #fff;
+    color: #333;
+    display: block;
+    font-size: 16px;
+    line-height: 66px;
+    padding: 0 15px;
+    margin: 0 2px;
+    transition: .3s;
+    -webkit-transition: .3s;
+  }
+
+  li a:hover {
+    background-color: #ff662f;
+    text-decoration: none;
+    color: #fff;
+  }
+
+  .search {
+    border: 1px solid #eee;
+    border-radius: 20px;
+    color: #666;
+    font-family: "Microsoft YaHei";
+    height: 30px;
+    line-height: 30px;
+    margin: 15px 0 0 0;
+    padding: 0 30px 0 15px;
+    transition: all 0.3s ease 0s;
+    width: 110px;
+    -webkit-transition: .3s;
+    background-color: #f9f9f9;
+  }
+</style>
